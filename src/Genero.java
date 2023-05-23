12a78cae19fef385ad368157ff3d0e71be64e9b4
SanguchoMela <melany01-03@hotmail.com>
2023-05-23 09:47:19 -0500

    cambios varios

--- src/Genero.java
+++ src/Genero.java
@@ -68,6 +68,7 @@ public class Genero {
     //---------------------------------
     //metodo para imprimir datos de cualquier objeto
     public void imprimirDatos(){
+        //Hola a todos
         System.out.println(this.tipo);
         System.out.println(this.ritmo);
         System.out.println(this.anio);
